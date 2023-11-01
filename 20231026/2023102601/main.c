929c05bc7a38606c2d4d3096ad45f9b84ca32332
Daniel Nazario <115184735+Malzinha0221@users.noreply.github.com>
2023-11-01 14:19:02 -0400

    Update main.c

--- 20231026/2023102601/main.c
+++ 20231026/2023102601/main.c
@@ -1,1 +1,15 @@
-// Aqui ficarao os arquivos main
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pessoa.c"
+#include "pessoa.h"
+int main()
+{
+    Pessoa *pessoa = (Pessoa *)malloc(sizeof(Pessoa));
+
+    Preencher_pessoa(pessoa);
+    Imprimir_pessoa(pessoa);
+    Libera_pessooa(pessoa);
+
+    return 0;
+}
