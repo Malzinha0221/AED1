bf8cebf0ea28cf33631c4457d0961c7619aeeb04
Daniel Nazario <115184735+Malzinha0221@users.noreply.github.com>
2023-11-01 14:19:50 -0400

    Update pessoa.h

--- 20231026/2023102601/pessoa.h
+++ 20231026/2023102601/pessoa.h
@@ -1,1 +1,16 @@
+#ifndef PESSOA_H_INCLUDED
+#define PESSOA_H_INCLUDED
 
+struct pessoa
+{
+    char nome[20];
+    char data_nascimento[11]; // XX/XX/XXXX
+    char cpf[15];             // xxx.xxx.xxx-xx
+};
+
+typedef struct pessoa Pessoa;
+// Colocar as funcões criadas
+void Preencher_pessoa(Pessoa *pessoa);
+void Libera_pessooa(Pessoa *pessoa);
+void Imprimir_pessoa(const Pessoa *pessoa);
+#endif // PESSOA_H_INCLUDED
