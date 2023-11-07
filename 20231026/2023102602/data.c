c7215769584dccccc10b25e0852c8e58f25b1d1f
Daniel Nazario <115184735+Malzinha0221@users.noreply.github.com>
2023-11-07 00:19:29 -0400

    Update data.c

--- 20231026/2023102602/data.c
+++ 20231026/2023102602/data.c
@@ -1,1 +1,27 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <string.h>
+#include "data.h"
 
+void preencherData(DATA *data){
+    data->dia=20;
+    data->mes=3;
+    data->ano=1998;
+}
+
+void ImprimirData(DATA *data){
+    printf("%02d/%02d/%04d", data->dia, data->mes, data->ano);
+}
+
+void preencherPessoa(PESSOA *pessoa){
+    strcpy(pessoa->nome,"Daniel Nazario");
+    preencherData(&pessoa->data_nascimento);
+    strcpy(pessoa->CPF, "123.456.789-11");
+}
+
+void ImprimirPessoa(PESSOA *pessoa){
+    printf("Nome: %s\n",pessoa->nome);
+    printf("CPF: %s\n",pessoa->CPF);
+    printf("Data de Nascimento: ");
+    ImprimirData(&pessoa->data_nascimento);
+}
