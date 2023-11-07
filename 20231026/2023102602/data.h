4fd3fce1f051b2eb7e0fb9343cf120dc617493d5
Daniel Nazario <115184735+Malzinha0221@users.noreply.github.com>
2023-11-07 00:19:49 -0400

    Update data.h

--- 20231026/2023102602/data.h
+++ 20231026/2023102602/data.h
@@ -1,1 +1,21 @@
+#ifndef DATA_H_INCLUCED
+#define DATA_H_INCLUDED
 
+typedef struct {
+    int dia;
+    int mes;
+    int ano;
+}DATA;
+
+typedef struct{
+    char nome[50];
+    DATA data_nascimento;
+    char CPF[15];
+}PESSOA;
+
+void preencherData(DATA *data);
+void ImprimirData(DATA *data);
+void preencherPessoa(PESSOA *pessoa);
+void ImprimirPessoa(PESSOA *pessoa);
+
+#endif // DATA_H_INCLUDED
