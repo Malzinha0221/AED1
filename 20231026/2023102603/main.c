a818cd50a3811ad8e8ec5dbc052364ea332d8089
Daniel Nazario <115184735+Malzinha0221@users.noreply.github.com>
2023-11-07 00:27:45 -0400

    Update main.c

--- 20231026/2023102603/main.c
+++ 20231026/2023102603/main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include "cubo.h"
+#include "cubo.c"
 
+int main() {
+    Cubo meuCubo;
+    InicializarCubo(&meuCubo, 3.0);
+
+    printf("Área do cubo: %.2lf unidades quadradas\n", CalcularArea(&meuCubo));
+    printf("Volume do cubo: %.2lf unidades cúbicas\n", CalcularVolume(&meuCubo));
+
+    return 0;
+}
