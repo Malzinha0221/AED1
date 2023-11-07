296b1269b525cbe4cf09d27d3314490b2c4e35cb
Daniel Nazario <115184735+Malzinha0221@users.noreply.github.com>
2023-11-07 00:18:57 -0400

    Update main.c

--- 20231026/2023102602/main.c
+++ 20231026/2023102602/main.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "data.h"
 
+int main() {
+    PESSOA *pessoa = (PESSOA *)malloc(sizeof(PESSOA));
+
+    preencherPessoa(pessoa);
+
+    ImprimirPessoa(pessoa);
+
+    free(pessoa);
+    return 0;
+}
