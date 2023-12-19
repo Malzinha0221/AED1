95ef43d0367619bdf03c9b9ca4eb586d9f0f66c6
Daniel Nazario <115184735+Malzinha0221@users.noreply.github.com>
2023-12-19 11:48:42 -0400

    Update main.c

--- 20231214/Execicio2/main.c
+++ 20231214/Execicio2/main.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lld.h"
+int main() {
+    ListaPolinomio polinomio1, polinomio2;
+    inicializarListaPolinomio(&polinomio1);
+    inicializarListaPolinomio(&polinomio2);
+
+    // Inserir alguns termos nos polinômios
+    inserirTermoOrdenado(&polinomio1, 3, 5);
+    inserirTermoOrdenado(&polinomio1, 2, 1);
+    inserirTermoOrdenado(&polinomio1, -1, 0);
+
+    inserirTermoOrdenado(&polinomio2, 6, 1);
+    inserirTermoOrdenado(&polinomio2, 12, 0);
+
+    // Imprimir os polinômios
+    printf("Polinômio 1: ");
+    imprimirPolinomio(&polinomio1);
+
+    printf("Polinômio 2: ");
+    imprimirPolinomio(&polinomio2);
+
+    // Somar os polinômios
+    ListaPolinomio resultado = somaPolinomios(&polinomio1, &polinomio2);
+
+    // Imprimir o resultado
+    printf("Resultado da soma: ");
+    imprimirPolinomio(&resultado);
+
+    return 0;
+}
 
